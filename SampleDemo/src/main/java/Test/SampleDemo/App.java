57210e8520ac634d84f0eceff7e5474596731c34
divya <divya@Divya>
2021-12-11 10:44:58 +0530

    update project

---
+++ SampleDemo/src/main/java/Test/SampleDemo/App.java
@@ -0,0 +1,13 @@
+package Test.SampleDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
